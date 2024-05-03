f61675baa5555ef9b2d4bed4ac1fc9278d6c662c
yanyukiai <78672128+yanyukiai@users.noreply.github.com>
2024-05-03 17:46:47 -0400

    Create button to add new book into library.

--- javascript.js
+++ javascript.js
@@ -26,6 +26,11 @@ function addBookToLibrary(book) {
 }
 
 function displayBooks(library, locAtPage) {
+  // Clear current display to avoid duplicate books in screen
+  while (locAtPage.firstChild) {
+    locAtPage.removeChild(locAtPage.firstChild);
+  }
+  // Display all books in library
   for (const item of library) {
     const bookCard = document.createElement("div");
     bookCard.classList.add("bookCard");
@@ -58,17 +63,67 @@ function displayBooks(library, locAtPage) {
   }
 }
 
-// Unit Test
-function displayArray(arrayToShow) {
-  arrayToShow.forEach((item) => console.log(item));
-}
-console.log("Initial Library:");
-displayArray(myLibrary);
-const book1 = new Book("River Town", "Peter Hessler", 432, true);
-// book1.info();
+// Initial Library
+const book1 = new Book("River Town", "Peter Hessler", 432, "Yes");
+const book2 = new Book("Oracle", "Peter Hessler", 500, "Yes");
 addBookToLibrary(book1);
-console.log("Current Library:");
-displayArray(myLibrary);
+addBookToLibrary(book2);
 
+// Display initial library
 const bookInLibrary = document.querySelector("#bookInLibrary");
 displayBooks(myLibrary, bookInLibrary);
+
+// Open new book dialog
+const newBookDialog = document.querySelector("#newBookDialog");
+const newBookBtn = document.querySelector("#newBookBtn");
+const confirmNewBookBtn = newBookDialog.querySelector("#confirmNewBookBtn");
+const cancelNewBookBtn = newBookDialog.querySelector("#cancelNewBookBtn");
+
+const bookTitle = document.querySelector("#bookTitle");
+const bookAuthor = document.querySelector("#bookAuthor");
+const bookPages = document.querySelector("#bookPages");
+const bookRead = document.querySelector("#bookRead");
+
+newBookBtn.addEventListener("click", () => {
+  // Clear current inputs
+  bookTitle.value = "";
+  bookAuthor.value = "";
+  bookPages.value = 0;
+  bookRead.value = "No";
+  // Open the dialog to input new book info
+  newBookDialog.showModal();
+});
+
+// Confirm to add a new book
+confirmNewBookBtn.addEventListener("click", (e) => {
+  if (bookTitle.value !== "" && bookAuthor.value !== "") {
+    e.preventDefault(); // No need to submit the form
+    const newBook = new Book(
+      bookTitle.value,
+      bookAuthor.value,
+      bookPages.value,
+      bookRead.value
+    );
+    // Add book to myLibrary
+    addBookToLibrary(newBook);
+    // Display library
+    displayBooks(myLibrary, bookInLibrary);
+    newBookDialog.close(); // close the dialog
+  }
+});
+
+cancelNewBookBtn.addEventListener("click", (e) => {
+  newBookDialog.close(); // bypass the validation check
+});
+
+// Unit Test
+// function displayArray(arrayToShow) {
+//   arrayToShow.forEach((item) => console.log(item));
+// }
+// console.log("Initial Library:");
+// displayArray(myLibrary);
+
+// book1.info();
+
+// console.log("Current Library:");
+// displayArray(myLibrary);
